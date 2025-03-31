8038055c2153e773a49be5b1051d37e02c20876d
prathshenoy <113072185+prathshenoy@users.noreply.github.com>
2025-03-31 18:07:23 -0400

    Update main.go

--- cmd/templ/main.go
+++ cmd/templ/main.go
@@ -1,394 +1,133 @@
-package main
+package lspcmd
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
-	"runtime"
 
-	"github.com/a-h/templ"
-	"github.com/a-h/templ/cmd/templ/fmtcmd"
-	"github.com/a-h/templ/cmd/templ/generatecmd"
-	"github.com/a-h/templ/cmd/templ/infocmd"
-	"github.com/a-h/templ/cmd/templ/lspcmd"
-	"github.com/a-h/templ/cmd/templ/sloghandler"
-	"github.com/fatih/color"
+	"github.com/a-h/templ/cmd/templ/lspcmd/httpdebug"
+	"github.com/a-h/templ/cmd/templ/lspcmd/pls"
+	"github.com/a-h/templ/cmd/templ/lspcmd/proxy"
+	"github.com/a-h/templ/lsp/jsonrpc2"
+	"github.com/a-h/templ/lsp/protocol"
+
+	_ "net/http/pprof"
 )
 
-func main() {
-	code := run(os.Stdin, os.Stdout, os.Stderr, os.Args)
-	if code != 0 {
-		os.Exit(code)
-	}
+type Arguments struct {
+	Log           string
+	GoplsLog      string
+	GoplsRPCTrace bool
+	// PPROF sets whether to start a profiling server on localhost:9999
+	PPROF bool
+	// HTTPDebug sets the HTTP endpoint to listen on. Leave empty for no web debug.
+	HTTPDebug string
+	// NoPreload disables preloading of templ files on server startup (useful for large monorepos)
+	NoPreload bool
 }
 
-const usageText = `usage: templ <command> [<args>...]
-
-templ - build HTML UIs with Go
-
-See docs at https://templ.guide
-
-commands:
-  generate   Generates Go code from templ files
-  fmt        Formats templ files
-  lsp        Starts a language server for templ files
-  info       Displays information about the templ environment
-  version    Prints the version
-`
-
-func run(stdin io.Reader, stdout, stderr io.Writer, args []string) (code int) {
-	if len(args) < 2 {
-		fmt.Fprint(stderr, usageText)
-		return 64 // EX_USAGE
-	}
-	switch args[1] {
-	case "info":
-		return infoCmd(stdout, stderr, args[2:])
-	case "generate":
-		return generateCmd(stdout, stderr, args[2:])
-	case "fmt":
-		return fmtCmd(stdin, stdout, stderr, args[2:])
-	case "lsp":
-		return lspCmd(stdin, stdout, stderr, args[2:])
-	case "version", "--version":
-		fmt.Fprintln(stdout, templ.Version())
-		return 0
-	case "help", "-help", "--help", "-h":
-		fmt.Fprint(stdout, usageText)
-		return 0
-	}
-	fmt.Fprint(stderr, usageText)
-	return 64 // EX_USAGE
-}
-
-func newLogger(logLevel string, verbose bool, stderr io.Writer) *slog.Logger {
-	if verbose {
-		logLevel = "debug"
-	}
-	level := slog.LevelInfo.Level()
-	switch logLevel {
-	case "debug":
-		level = slog.LevelDebug.Level()
-	case "warn":
-		level = slog.LevelWarn.Level()
-	case "error":
-		level = slog.LevelError.Level()
-	}
-	return slog.New(sloghandler.NewHandler(stderr, &slog.HandlerOptions{
-		AddSource: logLevel == "debug",
-		Level:     level,
-	}))
-}
-
-const infoUsageText = `usage: templ info [<args>...]
-
-Displays information about the templ environment.
-
-Args:
-  -json
-    Output information in JSON format to stdout. (default false)
-  -v
-    Set log verbosity level to "debug". (default "info")
-  -log-level
-    Set log verbosity level. (default "info", options: "debug", "info", "warn", "error")
-  -help
-    Print help and exit.
-`
-
-func infoCmd(stdout, stderr io.Writer, args []string) (code int) {
-	cmd := flag.NewFlagSet("diagnose", flag.ExitOnError)
-	jsonFlag := cmd.Bool("json", false, "")
-	verboseFlag := cmd.Bool("v", false, "")
-	logLevelFlag := cmd.String("log-level", "info", "")
-	helpFlag := cmd.Bool("help", false, "")
-	err := cmd.Parse(args)
-	if err != nil {
-		fmt.Fprint(stderr, infoUsageText)
-		return 64 // EX_USAGE
-	}
-	if *helpFlag {
-		fmt.Fprint(stdout, infoUsageText)
-		return
-	}
-
-	log := newLogger(*logLevelFlag, *verboseFlag, stderr)
-
-	ctx, cancel := context.WithCancel(context.Background())
+func Run(stdin io.Reader, stdout, stderr io.Writer, args Arguments) (err error) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
-	go func() {
-		<-signalChan
-		fmt.Fprintln(stderr, "Stopping...")
+	defer func() {
+		signal.Stop(signalChan)
 		cancel()
 	}()
-
-	err = infocmd.Run(ctx, log, stdout, infocmd.Arguments{
-		JSON: *jsonFlag,
-	})
-	if err != nil {
-		color.New(color.FgRed).Fprint(stderr, "(✗) ")
-		fmt.Fprintln(stderr, "Command failed: "+err.Error())
-		return 1
+	if args.PPROF {
+		go func() {
+			_ = http.ListenAndServe("localhost:9999", nil)
+		}()
 	}
-	return 0
+	go func() {
+		select {
+		case <-signalChan: // First signal, cancel context.
+			cancel()
+		case <-ctx.Done():
+		}
+		<-signalChan // Second signal, hard exit.
+		os.Exit(2)
+	}()
+	log := slog.New(slog.NewJSONHandler(io.Discard, nil))
+	if args.Log != "" {
+		file, err := os.OpenFile(args.Log, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		if err != nil {
+			return fmt.Errorf("failed to open log file: %w", err)
+		}
+		defer file.Close()
+
+		// Create a new logger with a file writer
+		log = slog.New(slog.NewJSONHandler(file, nil))
+		log.Debug("Logging to file", slog.String("file", args.Log))
+	}
+	templStream := jsonrpc2.NewStream(newStdRwc(log, "templStream", stdout, stdin))
+	return run(ctx, log, templStream, args)
 }
 
-const generateUsageText = `usage: templ generate [<args>...]
-
-Generates Go code from templ files.
-
-Args:
-  -path <path>
-    Generates code for all files in path. (default .)
-  -f <file>
-    Optionally generates code for a single file, e.g. -f header.templ
-  -stdout
-    Prints to stdout instead of writing generated files to the filesystem.
-    Only applicable when -f is used.
-  -source-map-visualisations
-    Set to true to generate HTML files to visualise the templ code and its corresponding Go code.
-  -include-version
-    Set to false to skip inclusion of the templ version in the generated code. (default true)
-  -include-timestamp
-    Set to true to include the current time in the generated code.
-  -watch
-    Set to true to watch the path for changes and regenerate code.
-  -watch-pattern <regexp>
-    Set the regexp pattern of files that will be watched for changes. (default: '(.+\.go$)|(.+\.templ$)|(.+_templ\.txt$)')
-  -cmd <cmd>
-    Set the command to run after generating code.
-  -proxy
-    Set the URL to proxy after generating code and executing the command.
-  -proxyport
-    The port the proxy will listen on. (default 7331)
-  -proxybind
-    The address the proxy will listen on. (default 127.0.0.1)
-  -notify-proxy
-    If present, the command will issue a reload event to the proxy 127.0.0.1:7331, or use proxyport and proxybind to specify a different address.
-  -w
-    Number of workers to use when generating code. (default runtime.NumCPUs)
-  -lazy
-    Only generate .go files if the source .templ file is newer.	
-  -pprof
-    Port to run the pprof server on.
-  -keep-orphaned-files
-    Keeps orphaned generated templ files. (default false)
-  -v
-    Set log verbosity level to "debug". (default "info")
-  -log-level
-    Set log verbosity level. (default "info", options: "debug", "info", "warn", "error")
-  -help
-    Print help and exit.
-
-Examples:
-
-  Generate code for all files in the current directory and subdirectories:
-
-    templ generate
-
-  Generate code for a single file:
-
-    templ generate -f header.templ
-
-  Watch the current directory and subdirectories for changes and regenerate code:
-
-    templ generate -watch
-`
-
-func generateCmd(stdout, stderr io.Writer, args []string) (code int) {
-	cmd := flag.NewFlagSet("generate", flag.ExitOnError)
-	fileNameFlag := cmd.String("f", "", "")
-	pathFlag := cmd.String("path", ".", "")
-	toStdoutFlag := cmd.Bool("stdout", false, "")
-	sourceMapVisualisationsFlag := cmd.Bool("source-map-visualisations", false, "")
-	includeVersionFlag := cmd.Bool("include-version", true, "")
-	includeTimestampFlag := cmd.Bool("include-timestamp", false, "")
-	watchFlag := cmd.Bool("watch", false, "")
-	watchPatternFlag := cmd.String("watch-pattern", "(.+\\.go$)|(.+\\.templ$)", "")
-	openBrowserFlag := cmd.Bool("open-browser", true, "")
-	cmdFlag := cmd.String("cmd", "", "")
-	proxyFlag := cmd.String("proxy", "", "")
-	proxyPortFlag := cmd.Int("proxyport", 7331, "")
-	proxyBindFlag := cmd.String("proxybind", "127.0.0.1", "")
-	notifyProxyFlag := cmd.Bool("notify-proxy", false, "")
-	workerCountFlag := cmd.Int("w", runtime.NumCPU(), "")
-	pprofPortFlag := cmd.Int("pprof", 0, "")
-	keepOrphanedFilesFlag := cmd.Bool("keep-orphaned-files", false, "")
-	verboseFlag := cmd.Bool("v", false, "")
-	logLevelFlag := cmd.String("log-level", "info", "")
-	lazyFlag := cmd.Bool("lazy", false, "")
-	helpFlag := cmd.Bool("help", false, "")
-	err := cmd.Parse(args)
-	if err != nil {
-		fmt.Fprint(stderr, generateUsageText)
-		return 64 // EX_USAGE
-	}
-	if *helpFlag {
-		fmt.Fprint(stdout, generateUsageText)
-		return
-	}
-
-	log := newLogger(*logLevelFlag, *verboseFlag, stderr)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	go func() {
-		<-signalChan
-		fmt.Fprintln(stderr, "Stopping...")
-		cancel()
+func run(ctx context.Context, log *slog.Logger, templStream jsonrpc2.Stream, args Arguments) (err error) {
+	log.Info("lsp: starting up...")
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("handled panic", slog.Any("recovered", r))
+		}
 	}()
 
-	var fw generatecmd.FileWriterFunc
-	if *toStdoutFlag {
-		fw = generatecmd.WriterFileWriter(stdout)
-	}
-
-	err = generatecmd.Run(ctx, log, generatecmd.Arguments{
-		FileName:                        *fileNameFlag,
-		Path:                            *pathFlag,
-		FileWriter:                      fw,
-		Watch:                           *watchFlag,
-		WatchPattern:                    *watchPatternFlag,
-		OpenBrowser:                     *openBrowserFlag,
-		Command:                         *cmdFlag,
-		Proxy:                           *proxyFlag,
-		ProxyPort:                       *proxyPortFlag,
-		ProxyBind:                       *proxyBindFlag,
-		NotifyProxy:                     *notifyProxyFlag,
-		WorkerCount:                     *workerCountFlag,
-		GenerateSourceMapVisualisations: *sourceMapVisualisationsFlag,
-		IncludeVersion:                  *includeVersionFlag,
-		IncludeTimestamp:                *includeTimestampFlag,
-		PPROFPort:                       *pprofPortFlag,
-		KeepOrphanedFiles:               *keepOrphanedFilesFlag,
-		Lazy:                            *lazyFlag,
+	log.Info("lsp: starting gopls...")
+	rwc, err := pls.NewGopls(ctx, log, pls.Options{
+		Log:      args.GoplsLog,
+		RPCTrace: args.GoplsRPCTrace,
 	})
 	if err != nil {
-		color.New(color.FgRed).Fprint(stderr, "(✗) ")
-		fmt.Fprintln(stderr, "Command failed: "+err.Error())
-		return 1
-	}
-	return 0
-}
-
-const fmtUsageText = `usage: templ fmt [<args> ...]
-
-Format all files in directory:
-
-  templ fmt .
-
-Format stdin to stdout:
-
-  templ fmt < header.templ
-
-Format file or directory to stdout:
-
-  templ fmt -stdout FILE
-
-Args:
-  -stdout
-    Prints to stdout instead of in-place format
-  -stdin-filepath
-    Provides the formatter with filepath context when using -stdout.
-    Required for organising imports.
-  -v
-    Set log verbosity level to "debug". (default "info")
-  -log-level
-    Set log verbosity level. (default "info", options: "debug", "info", "warn", "error")
-  -w
-    Number of workers to use when formatting code. (default runtime.NumCPUs).
-  -fail
-    Fails with exit code 1 if files are changed. (e.g. in CI)
-  -help
-    Print help and exit.
-`
-
-func fmtCmd(stdin io.Reader, stdout, stderr io.Writer, args []string) (code int) {
-	cmd := flag.NewFlagSet("fmt", flag.ExitOnError)
-	helpFlag := cmd.Bool("help", false, "")
-	workerCountFlag := cmd.Int("w", runtime.NumCPU(), "")
-	verboseFlag := cmd.Bool("v", false, "")
-	logLevelFlag := cmd.String("log-level", "info", "")
-	failIfChanged := cmd.Bool("fail", false, "")
-	stdoutFlag := cmd.Bool("stdout", false, "")
-	stdinFilepath := cmd.String("stdin-filepath", "", "")
-	err := cmd.Parse(args)
-	if err != nil {
-		fmt.Fprint(stderr, fmtUsageText)
-		return 64 // EX_USAGE
-	}
-	if *helpFlag {
-		fmt.Fprint(stdout, fmtUsageText)
-		return
+		log.Error("failed to start gopls", slog.Any("error", err))
+		os.Exit(1)
 	}
 
-	log := newLogger(*logLevelFlag, *verboseFlag, stderr)
+	cache := proxy.NewSourceMapCache()
+	diagnosticCache := proxy.NewDiagnosticCache()
 
-	err = fmtcmd.Run(log, stdin, stdout, fmtcmd.Arguments{
-		ToStdout:      *stdoutFlag,
-		Files:         cmd.Args(),
-		WorkerCount:   *workerCountFlag,
-		StdinFilepath: *stdinFilepath,
-		FailIfChanged: *failIfChanged,
-	})
-	if err != nil {
-		return 1
-	}
-	return 0
-}
+	log.Info("creating gopls client")
+	clientProxy, clientInit := proxy.NewClient(log, cache, diagnosticCache)
+	_, goplsConn, goplsServer := protocol.NewClient(ctx, clientProxy, jsonrpc2.NewStream(rwc), log)
+	defer goplsConn.Close()
 
-const lspUsageText = `usage: templ lsp [<args> ...]
+	log.Info("creating proxy")
+	// Create the proxy to sit between.
+	serverProxy := proxy.NewServer(log, goplsServer, cache, diagnosticCache, args.NoPreload)
 
-Starts a language server for templ.
+	// Create templ server.
+	log.Info("creating templ server")
+	_, templConn, templClient := protocol.NewServer(context.Background(), serverProxy, templStream, log)
+	defer templConn.Close()
 
-Args:
-  -log string
-    The file to log templ LSP output to, or leave empty to disable logging.
-  -goplsLog string
-    The file to log gopls output, or leave empty to disable logging.
-  -goplsRPCTrace
-    Set gopls to log input and output messages.
-  -help
-    Print help and exit.
-  -pprof
-    Enable pprof web server (default address is localhost:9999)
-  -http string
-    Enable http debug server by setting a listen address (e.g. localhost:7474)
-`
+	// Allow both the server and the client to initiate outbound requests.
+	clientInit(templClient)
 
-func lspCmd(stdin io.Reader, stdout, stderr io.Writer, args []string) (code int) {
-	cmd := flag.NewFlagSet("lsp", flag.ExitOnError)
-	logFlag := cmd.String("log", "", "")
-	goplsLog := cmd.String("goplsLog", "", "")
-	goplsRPCTrace := cmd.Bool("goplsRPCTrace", false, "")
-	helpFlag := cmd.Bool("help", false, "")
-	pprofFlag := cmd.Bool("pprof", false, "")
-	httpDebugFlag := cmd.String("http", "", "")
-	err := cmd.Parse(args)
-	if err != nil {
-		fmt.Fprint(stderr, lspUsageText)
-		return 64 // EX_USAGE
-	}
-	if *helpFlag {
-		fmt.Fprint(stdout, lspUsageText)
-		return
+	// Start the web server if required.
+	if args.HTTPDebug != "" {
+		log.Info("starting debug http server", slog.String("addr", args.HTTPDebug))
+		h := httpdebug.NewHandler(log, serverProxy)
+		go func() {
+			if err := http.ListenAndServe(args.HTTPDebug, h); err != nil {
+				log.Error("web server failed", slog.Any("error", err))
+			}
+		}()
 	}
 
-	err = lspcmd.Run(stdin, stdout, stderr, lspcmd.Arguments{
-		Log:           *logFlag,
-		GoplsLog:      *goplsLog,
-		GoplsRPCTrace: *goplsRPCTrace,
-		PPROF:         *pprofFlag,
-		HTTPDebug:     *httpDebugFlag,
-	})
-	if err != nil {
-		fmt.Fprintln(stderr, err.Error())
-		return 1
+	log.Info("listening")
+
+	select {
+	case <-ctx.Done():
+		log.Info("context closed")
+	case <-templConn.Done():
+		log.Info("templConn closed")
+	case <-goplsConn.Done():
+		log.Info("goplsConn closed")
 	}
-	return 0
+	log.Info("shutdown complete")
+	return
 }
